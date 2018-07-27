b3b4c52bb9bc7f81c5c08047d98ded0a22483992
syoya00 <tkd0617f@gmail.com>
2018-07-27 13:33:26 +0900

    mousePress、カウント処理

--- some_games_01.js
+++ some_games_01.js
@@ -1,7 +1,9 @@
 function sketchProc(processing) {
  
     var y = 100;
- 
+    var count = 0;
+    var move = false;
+
     processing.setup = function() {
         processing.size(innerWidth,innerHeight);
         processing.frameRate(30);
@@ -22,8 +24,24 @@ function sketchProc(processing) {
         processing.background(0);
         processing.noFill();
         processing.strokeWeight(1);
+        if(move){
+            processing.strokeWeight(3);
+        }
         processing.stroke(255);
         processing.ellipse(processing.mouseX,processing.mouseY,30,30);
+
+        if(processing.mousePressed){
+            move = true;
+        }else if(processing.mouseReleased){
+            move = false;
+        }
+
+        if(move){
+            count++;
+            processing.fill(255);
+            processing.textSize(30);
+            processing.text(count,100,100);
+        }
 
     }
  
